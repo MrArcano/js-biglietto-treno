7f80bb93f9ebdc8ff96fd6938e3eb19f7202e40a
MrArcano <salvatore-dario@hotmail.it>
2023-09-07 14:13:51 +0200

    [add]: app point

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+/*
+
+1. Chiedere numero di chilometri da percorrere ed età
+
+2. Calcolo tariffa chilometri
+
+3. Controllo età e applico sconto se necessario
+
+4. Output messaggio cifra finale con due decimali
+
+*/
